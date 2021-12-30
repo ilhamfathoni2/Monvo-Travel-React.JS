f8aedafdc6029f04ea611c4024cde5f9db9a05fc
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-12-30 20:05:48 +0700

    Install React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useContext } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { UserContext } from "./context/userContext";
+import { API, setAuthToken } from "./config/api";
+
+import LogIn from "./components/auth/login";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/auth/login.js
@@ -0,0 +1,16 @@
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import { API } from "../../config/api";
+import { Button, Form, Container, Card, Alert } from "react-bootstrap";
+import "./auth.css";
+
+function LogIn() {
+  return (
+    <>
+      <div>hello</div>
+    </>
+  );
+}
+
+export default LogIn;
