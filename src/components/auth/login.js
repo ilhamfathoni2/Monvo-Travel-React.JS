594b0b2303eabc18b52709c99994438125851fb4
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-12-30 21:02:55 +0700

    Testing Form validation with Formik & Yup

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -5,10 +5,59 @@ import { API } from "../../config/api";
 import { Button, Form, Container, Card, Alert } from "react-bootstrap";
 import "./auth.css";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 function LogIn() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email format").required("Required!"),
+      password: Yup.string()
+        .min(8, "Minimum 8 characters")
+        .required("Required!"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <>
-      <div>hello</div>
+      <Container className="py-5">
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              name="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+            />
+            {formik.errors.email && formik.touched.email && (
+              <p className="text-danger">{formik.errors.email}</p>
+            )}
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+            {formik.errors.password && formik.touched.password && (
+              <p className="text-danger">{formik.errors.password}</p>
+            )}
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Container>
     </>
   );
 }
